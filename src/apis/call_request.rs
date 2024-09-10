b4689b4ec94f40eeca00a23f3d70349ee4f881d4
louis013 <angelo.louis13@gmail.com>
2024-09-10 15:30:59 +0800

    Created client and chat completion

--- src/apis/call_request.rs
+++ src/apis/call_request.rs
@@ -1,7 +1,9 @@
-use crate::models::general::llm::Message;
+use crate::models::general::llm::{ Message, ChatCompletion };
 use dotenv::dotenv;
 use reqwest::Client;
 use std::env;
+
+use reqwest::header::{ HeaderMap, HeaderValue };
 
 // Call Large Language Model (GPT-3.5 - Turbo)
 pub async fn call_gpt(messages: Vec<Message>) {
@@ -13,4 +15,42 @@ pub async fn call_gpt(messages: Vec<Message>) {
 
     // Confirm endpoint
     let url: &str = "https://api.openai.com/v1/chat/completions";
+
+    // Create headers
+    let mut headers = HeaderMap::new();
+
+    // Create api key header
+    headers.insert(
+        "authorization", 
+        HeaderValue::from_str(&format!("Bearer {}", api_key)).unwrap()
+    );
+
+    // Create Open AI Org header
+    headers.insert(
+        "OpenAI-Organization", 
+        HeaderValue::from_str(api_org.as_str()).unwrap()
+    );
+
+    // Create client
+    let client = Client::builder()
+        .default_headers(headers)
+        .build()
+        .unwrap();
+
+    // Create chat completion
+    let chat_completion: ChatCompletion = ChatCompletion {
+        model: "gpt-3.5-turbo".to_string(),
+        messages,
+        temperature: 0.1
+    };
+
+    // Troubleshooting
+    let res_raw = client
+        .post(url)
+        .json(&chat_completion)
+        .send()
+        .await
+        .unwrap();
+
+    dbg!(res_raw.text().await.unwrap());
 }
