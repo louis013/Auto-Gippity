5fd6ea318de96e75dee6745c2d3d2d08130575fe
louis013 <angelo.louis13@gmail.com>
2024-09-16 09:04:48 +0800

    Modified error handling

--- src/apis/call_request.rs
+++ src/apis/call_request.rs
@@ -1,12 +1,12 @@
 use crate::models::general::llm::{ Message, ChatCompletion };
 use dotenv::dotenv;
-use reqwest::Client;
+use reqwest::{header::InvalidHeaderValue, Client};
 use std::env;
 
 use reqwest::header::{ HeaderMap, HeaderValue };
 
 // Call Large Language Model (GPT-3.5 - Turbo)
-pub async fn call_gpt(messages: Vec<Message>) {
+pub async fn call_gpt(messages: Vec<Message>) -> Result<String, Box<dyn std::error::Error + Send>> {
     dotenv().ok();
 
     // Extract API Key information
@@ -22,20 +22,22 @@ pub async fn call_gpt(messages: Vec<Message>) {
     // Create api key header
     headers.insert(
         "authorization", 
-        HeaderValue::from_str(&format!("Bearer {}", api_key)).unwrap()
+        HeaderValue::from_str(&format!("Bearer {}", api_key))
+            .map_err(|e: InvalidHeaderValue| -> Box<dyn std::error::Error + Send> { Box::new(e)})?
     );
 
     // Create Open AI Org header
     headers.insert(
         "OpenAI-Organization", 
-        HeaderValue::from_str(api_org.as_str()).unwrap()
+        HeaderValue::from_str(api_org.as_str())
+            .map_err(|e: InvalidHeaderValue| -> Box<dyn std::error::Error + Send> { Box::new(e)})?
     );
 
     // Create client
-    let client = Client::builder()
+    let client: Client = Client::builder()
         .default_headers(headers)
         .build()
-        .unwrap();
+        .map_err(|e| -> Box<dyn std::error::Error + Send> { Box::new(e) })?;
 
     // Create chat completion
     let chat_completion: ChatCompletion = ChatCompletion {
@@ -45,14 +47,17 @@ pub async fn call_gpt(messages: Vec<Message>) {
     };
 
     // Troubleshooting
-    let res_raw = client
-        .post(url)
-        .json(&chat_completion)
-        .send()
-        .await
-        .unwrap();
+    // let res_raw = client
+    //     .post(url)
+    //     .json(&chat_completion)
+    //     .send()
+    //     .await
+    //     .unwrap();
 
-    dbg!(res_raw.text().await.unwrap());
+    // dbg!(res_raw.text().await.unwrap());
+
+    // Placeholder
+    Ok("some_string".to_string())
 }
 
 #[cfg(test)]
