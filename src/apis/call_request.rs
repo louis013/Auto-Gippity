febb0f5ece5e486565c54aefc4b39d17575f8feb
louis013 <angelo.louis13@gmail.com>
2024-10-04 14:56:52 +0800

    Changed gpt model to gpt-4

--- src/apis/call_request.rs
+++ src/apis/call_request.rs
@@ -41,7 +41,7 @@ pub async fn call_gpt(messages: Vec<Message>) -> Result<String, Box<dyn std::err
 
     // Create chat completion
     let chat_completion: ChatCompletion = ChatCompletion {
-        model: "gpt-4o-mini".to_string(),
+        model: "gpt-4".to_string(),
         messages,
         temperature: 0.1
     };
