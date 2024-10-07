75d2fe506e67be4a770739d021cb5618abfc16f0
louis013 <angelo.louis13@gmail.com>
2024-10-07 11:21:59 +0800

    Cretaed read_exec_main_contents function

--- src/helpers/general.rs
+++ src/helpers/general.rs
@@ -81,6 +81,12 @@ pub fn read_code_template_contents() -> String {
     fs::read_to_string(path).expect("Failed to read code template")
 }
 
+// Get Exec Main
+pub fn read_exec_main_contents() -> String {
+    let path: String = String::from(EXEC_MAIN_PATH);
+    fs::read_to_string(path).expect("Failed to read exec main content")
+}
+
 // Save New Backend Code
 pub fn save_backend_code(contents: &String) {
     let path: String = String::from(EXEC_MAIN_PATH);
