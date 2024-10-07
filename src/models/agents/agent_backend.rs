291b9aa405f0a0e334c1762eab75bc532ca30afc
louis013 <angelo.louis13@gmail.com>
2024-10-07 11:16:29 +0800

    Created call_fix_code_bugs function

--- src/models/agents/agent_backend.rs
+++ src/models/agents/agent_backend.rs
@@ -80,4 +80,24 @@ impl AgentBackendDeveloper {
         save_backend_code(&ai_response);
         factsheet.backend_code = Some(ai_response);
     }
+
+    async fn call_fix_code_bugs(&mut self, factsheet: &mut FactSheet) {
+        let msg_context: String = format!(
+            "BROKEN_CODE: {:?} \n ERROR_BUGS: {:?} \n
+      THIS FUNCTION ONLY OUTPUTS CODE. JUST OUTPUT THE CODE.",
+            factsheet.backend_code, self.bug_errors
+        );
+
+        let ai_response: String = ai_task_request(
+            msg_context,
+            &self.attributes.position,
+            get_function_string!(print_fixed_code),
+            print_fixed_code,
+        )
+        .await;
+
+        save_backend_code(&ai_response);
+        factsheet.backend_code = Some(ai_response);
+    }
+
 }
